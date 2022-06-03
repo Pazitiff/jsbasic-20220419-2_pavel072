f5fe24e5d3f21e7e69d6b3508d4a6f4802e55f59
Pavel Popov <p.popov@roseltorg.ru>
2022-06-03 10:19:30 +0300

    Решение задачи 2 модуль 8

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -1,9 +1,64 @@
 import createElement from '../../assets/lib/create-element.js';
-import ProductCard from '../../6-module/2-task/index.js';
 
 export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.elem = this.render();
   }
+
+  render() {
+    const productGrid = document.createElement('DIV');
+    productGrid.classList.add('products-grid');
+    const productGridInner = document.createElement('DIV');
+    productGridInner.classList.add('products-grid__inner');
+    for (let product of this.products) {
+      productGridInner.append(this.createProductCard(product));
+    }
+    productGrid.append(productGridInner);
+    return productGrid;
+  }
+
+  createProductCard(product) {
+    let card = `<div class="card">
+    <div class="card__top">
+        <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+        <span class="card__price">€${product.price.toFixed(2)}</span>
+    </div>
+    <div class="card__body">
+        <div class="card__title">${product.name}</div>
+        <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+    </div>
+</div>`;
+    return createElement(card);
+  }
+
+  updateFilter(filters) {
+    for (let value in filters) {
+      if (filters.hasOwnProperty(value)) {
+        this.filters[value] = filters[value];
+      }
+    }
+
+    let grid = document.querySelector('.products-grid__inner');
+    grid.innerHTML = "";
+    for (let product of this.products) {
+      if (this.filters.noNuts && product.nuts) {
+        continue;
+      }
+      if (this.filters.vegeterianOnly && !product.vegeterian) {
+        continue;
+      }
+      if (void 0 !== this.filters.maxSpiciness && product.spiciness > this.filters.maxSpiciness) {
+        continue;
+      }
+      if (this.filters.category && product.category !== this.filters.category) {
+        continue;
+      }
+      grid.append(this.createProductCard(product));
+    }
+  }
+
 }
